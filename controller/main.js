3991d5877b092942c700361957b380f60226bc00
brunoksato <bruno.sato@live.com>
2014-02-04 00:35:03 -0200

    controller and scope

---
+++ controller/main.js
@@ -0,0 +1,8 @@
+
+/**
+ * [FirstCtrl controller]
+ * @param {[type]} $scope [referente ao html que esta dentro do controller firstCtrl]
+ */
+function FirstCtrl($scope){
+  $scope.data = {message: "Hello"};
+}
